74b8dcf45994371f87143ef358dcbba4e15b2341
Rene Rivas <tu_correo@ejemplo.com>
2026-09-15 18:46:09 -0300

    Configura proyecto Maven con JUnit y Selenium

---
+++ src/main/java/cl/iplacex/App.java
@@ -0,0 +1,13 @@
+package cl.iplacex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cl/iplacex/AppTest.java
@@ -0,0 +1,14 @@
+package cl.iplacex;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void pruebaSuma() {
+        int resultado = 2 + 3;
+
+        assertEquals(5, resultado);
+    }
+}
